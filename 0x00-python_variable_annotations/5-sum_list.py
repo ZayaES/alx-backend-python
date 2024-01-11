f5d1e0cbb7b95eaf37e7b1615b86428459118d4d
Zaya <heavenphrince@gmail.com>
2024-01-12 00:13:04 +0100

    5 2nd changed pi:int to pi:float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""contains sum_list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sums up the values of the list"""
+
+    return float(sum(input_list))
