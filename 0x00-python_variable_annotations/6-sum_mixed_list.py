7599c95d12177a6fb9ad7a5f306d4fb4f75c5bb8
Zaya <heavenphrince@gmail.com>
2024-01-12 00:27:01 +0100

    5 1st:float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""contains sum_mixed_list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """sum entries of mixed list"""
+
+    return float(sum(mxd_list))
