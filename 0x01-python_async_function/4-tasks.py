f1d4d7a860098744792828fbeb1ee24711a43898
Zaya <heavenphrince@gmail.com>
2024-01-17 23:42:32 +0100

    3, 4 -1st

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" contains a coroutine that runs concurrently with wait_random"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """a coroutine that runs with wait_random"""
+
+    result = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+    return sorted(result)
