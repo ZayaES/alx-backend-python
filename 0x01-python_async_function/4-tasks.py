faec1c3ec8b9efdbda798e4629679199ddffaf13
Zaya <heavenphrince@gmail.com>
2024-01-17 23:44:48 +0100

    4 -2nd pcs compliance, reduce line 13 to be less 79 characters

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,5 +10,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """a coroutine that runs with wait_random"""
 
-    result = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+    result = await asyncio.gather(*(
+             task_wait_random(max_delay) for i in range(n)))
     return sorted(result)
