08b4878b51818bf0d3a44d46baae0b1274776f8b
Zaya <heavenphrince@gmail.com>
2024-03-29 21:16:49 +0100

    task 2 test2 pycodestyle compliant

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import time
-from importlib import import_module
+from importlib import import_module as use
 
 
-async_comprehension = import_module('1-async_comprehension').async_comprehension
+async_comprehension = use('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
@@ -18,4 +18,3 @@ async def measure_runtime() -> float:
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time() - start_time
-
